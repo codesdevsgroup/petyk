8d3f5a871d64bdacea9466cf72784b4ef953278a
Fabiano Júnior <50252807+fabianosantos23@users.noreply.github.com>
2022-09-21 23:46:52 -0300

    side menu slide on click

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,6 +3,7 @@ function showMenu() {
   let menu = document.querySelector('.menu-options') 
 
   if (menu.classList.contains('active_menu')) {
+    menu.style.name = "slideout"
     menu.classList.remove('active_menu')
   } else {
     menu.classList.add('active_menu')
