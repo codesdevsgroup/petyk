86e6c7f8eeecbae3c0d094af993bb463e1a32d50
Fabiano Júnior <50252807+fabianosantos23@users.noreply.github.com>
2022-10-09 12:23:40 -0300

    back top button and adjsutments css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,18 @@
 ﻿window.addEventListener('scroll', onScroll)
 
-const home = document.getElementById('home')
-const nav = document.getElementById('navigation')
-
 onScroll()
 function onScroll() {
-  showNavOnScroll()
-  activateMenuAtCurrentSection(home)
+  // showNavOnScroll()
+  showBackTopButtonOnScroll()
 }
 
-function activateMenuAtCurrentSection(section) {
-  const targetLine = scrollY + innerHeight / 3
-
-  //verificar se a seção passou da linha
-  //quais dados vou precisar?
-
-  const sectionTop = section.offsetTop
-
-  const sectionHeight = section.offsetHeight
-
-  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
-
-  //  'O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetline
-  //verificar se a base está abaixo da linha alvo
-  //quais dados vou precisar?
-
-  const sectionEndsAt = sectionTop + sectionHeight
-
-  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
-
-  //console.log(
-  //  'O fundo da seção passou da linha?', sectionEndPassedTargetLine
-  //)
-
-  const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetLine
-  
-  const sectionId = document.getElementById('menuButton')
-
-  sectionId.classList.remove('active')
-  if (!sectionBoundaries) {
-    sectionId.classList.add('active')
-  }
-
-}
-
-function showNavOnScroll() {
-  if (window.scrollY > 199) {
-    nav.classList.add("scroll")
+function showBackTopButtonOnScroll() {
+  if (window.scrollY > 600) {
+    $('#backToTopButton').show(200)
   } else {
-    nav.classList.remove("scroll")
+    $('#backToTopButton').hide(200)
   }
 }
-
-/* function openMenu() {
-  document.body.classList.add('menu-expanded')
-}
-
-function closeMenu() {
-  document.body.classList.remove('menu-expanded')
-} */
 
 window.sr = ScrollReveal({ reset: true })
 
