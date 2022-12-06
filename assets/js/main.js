303082f36a4740eb7e157ae94a11e8b2728f9598
Fabiano Júnior <fabianojunior77@gmail.com>
2022-12-06 13:33:14 -0300

    on ready add iframe

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,18 @@ function showBackTopButtonOnScroll() {
     $('#backToTopButton').hide(200)
   }
 }
+
+
+$(document).ready(function () {
+  $('#video').html(`
+  <iframe class="embed-responsive-item"
+  width="848" 
+  height="480" 
+  src="https://www.youtube.com/embed/GQyBj67gKvs" 
+  title="YouTube video player"     
+  frameborder="0" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+  allowfullscreen>
+  </iframe>
+  `)
+})
